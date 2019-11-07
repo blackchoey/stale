2df9588756f1ef9a40d43836aa3d7a7612809d36
Chaoyi Yuan <blackchoey@users.noreply.github.com>
2019-11-07 16:31:16 +0800

    update

--- src/githubRepo.ts
+++ src/githubRepo.ts
@@ -8,12 +8,12 @@ type Issue = Octokit.IssuesListForRepoResponseItem;
 export class GithubRepo {
     private _client: github.GitHub;
     private _collaborators: string[];
-    private _eventsToCheck: string[];
+    private _eventsFromCollaborators: string[];
     private _ignoreEventsFromBot: boolean = true;
 
     constructor(client: github.GitHub, eventsToCheck: string[]) {
         this._client = client;
-        this._eventsToCheck = eventsToCheck;
+        this._eventsFromCollaborators = eventsToCheck;
         this._collaborators = [];
     }
 
@@ -65,17 +65,23 @@ export class GithubRepo {
         const allComments = await this.getAllCommentsForIssue(issue);
         operations += allComments.operations;
         let issueTimeline = allComments.result;
-        if (this._eventsToCheck.length > 0) {
+        if (this._eventsFromCollaborators.length > 0) {
             const allEvents = await this.getAllEventsForIssue(issue);
             operations += allEvents.operations;
             issueTimeline.push(...allEvents.result)
         }
 
-        const latestEvent = issueTimeline.reduce((prev, current) =>
-            prev.eventTime > current.eventTime ? prev : current
-        );
+        let lastUpdatedUser: string;
+        if (issueTimeline.length === 0) {
+            lastUpdatedUser = issue.user.login;
+        } else {
+            const lastEvent = issueTimeline.reduce((prev, current) =>
+                prev.eventTime > current.eventTime ? prev : current
+            );
+            lastUpdatedUser = lastEvent.actor;
+        }
 
-        if (this._collaborators.indexOf(latestEvent.actor) !== -1) {
+        if (this._collaborators.indexOf(lastUpdatedUser) !== -1) {
             return {
                 result: true,
                 operations: operations
@@ -112,6 +118,7 @@ export class GithubRepo {
     }
 
     private async getAllEventsForIssue(issue: Issue): Promise<{ result: IssueEvent[], operations: number }> {
+        let totalOperations = await this.ensureCollaboratorList();
         const allEvents = await this.getAllResult(async (page) => {
             return await this._client.issues.listEvents({
                 owner: github.context.repo.owner,
@@ -121,28 +128,33 @@ export class GithubRepo {
                 page: page
             })
         });
+        totalOperations += allEvents.operations;
 
         let issueEvents: IssueEvent[] = [];
         for (var event of allEvents.result) {
             if (!this._ignoreEventsFromBot || event.actor.type !== "Bot") {
-                if (this._eventsToCheck.indexOf(event.event) !== -1) {
+                if (this._eventsFromCollaborators.indexOf(event.event) !== -1) {
+                    let issueEvent: IssueEvent;
                     switch (event.event) {
                         case "assigned":
                         case "unassigned":
                             // TODO: Get actual assigner as actor when github client returns required info in the future.
                             let actor = event.actor.login;
-                            issueEvents.push(new IssueEvent(actor, event.event, new Date(event.created_at)));
+                            issueEvent = new IssueEvent(actor, event.event, new Date(event.created_at));
                             break;
                         default:
-                            issueEvents.push(new IssueEvent(event.actor.login, event.event, new Date(event.created_at)));
+                            issueEvent = new IssueEvent(event.actor.login, event.event, new Date(event.created_at));
                             break;
+                    }
+                    if (this._collaborators.indexOf(issueEvent.actor) !== -1) {
+                        issueEvents.push(issueEvent);
                     }
                 }
             }
         }
         return {
             result: issueEvents,
-            operations: allEvents.operations
+            operations: totalOperations
         };
     }
 
